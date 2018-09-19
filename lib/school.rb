ba161955c0d9294306fff2e2ebca6e8a9202b64c
soohoo05 <tyler.soohoo@gmail.com>
2018-09-18 23:05:06 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+require 'pry'
+class School
+
+
+def initialize(school_name)
+  @school_name=school_name
+  @roster={}
+end
+
+def roster
+  return @roster
+end
+
+def add_student(name,grade)
+
+if @roster[grade]==nil
+  @roster[grade]=[]
+  @roster[grade]<<name
+else
+  @roster[grade]<<name
+end
+end
+
+def grade (grade)
+return @roster[grade]
+end
+
+def sort
+  Hash[@roster.sort]
+  @roster.each do |key,value|
+    value.sort!
+  end
+
+
+end
+end
