df136ecf97b39f569fb6f1038e3ce50ef4cea806
github username <github email address>
2018-09-19 17:15:28 -0400

    student

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+
+
+class School
+
+attr_accessor :name, :shool, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+# binding.pry
+end
+
+def add_student(student_name, grade)
+  (roster[grade] ||= []) << student_name
+
+  # binding.pry
+end
+
+
+end
