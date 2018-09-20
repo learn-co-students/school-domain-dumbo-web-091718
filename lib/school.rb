b92ffe5efd779d3cabe6a6ba2445f2642d853591
github username <github email address>
2018-09-19 21:30:36 -0400

    adding WIP

--- lib/school.rb
+++ lib/school.rb
@@ -1,6 +1,4 @@
 # code here!
-
-
 class School
 
 attr_accessor :name, :shool, :roster
@@ -13,9 +11,22 @@ end
 
 def add_student(student_name, grade)
   (roster[grade] ||= []) << student_name
+  # p (roster[10] ||= [] << student_name)
 
   # binding.pry
 end
 
+def grade
+  if roster[] = [grade]
+  else
+    rooster[grade]  = [student]
+  end 
+end
+
+def sort
+  student_name.each do |a, b|
+    a.sort!
+  end
+end
 
 end
