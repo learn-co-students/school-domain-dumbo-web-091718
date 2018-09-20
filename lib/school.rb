8ded27eb83070e55e249515309307b2b684fcfd3
jasonodoom <jason@ohdoom.net>
2018-09-20 11:17:32 -0400

    wip

--- lib/school.rb
+++ lib/school.rb
@@ -17,9 +17,26 @@ class School
   def add_student(student_name, student_grade)
     # @student_name = student_name
     # @student_grade = student_grade
-    @roster[student_grade] = [student_name]
-    binding.pry
+    if @roster.keys.include?(student_grade)
+      @roster[student_grade] << student_name
+    else
+      @roster[student_grade] = [student_name]
+    end
+  end
 
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    sortArr = []
+    @roster.values.each do |students|
+     sortArr <<  students.sort
+     # binding.pry
+    # @roster.values.sort
+    # binding.pry
+    end
+    return sortArr
   end
 
 end
