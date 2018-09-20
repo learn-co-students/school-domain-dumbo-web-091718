640eac9ee4da4a690536ec9ecc1e7cdfc4d12354
jasonodoom <jason@ohdoom.net>
2018-09-19 23:07:34 -0400

    work in progress

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+require 'pry'
+class School
+
+  def roster
+    @roster
+  end
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def name
+    @name
+  end
+
+  def add_student(student_name, student_grade)
+    # @student_name = student_name
+    # @student_grade = student_grade
+    @roster[student_grade] = [student_name]
+    binding.pry
+
+  end
+
+end
+
+school = School.new("Bayside High School")
