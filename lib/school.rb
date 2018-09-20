0403fd790e01b01451e992a7842ce36dac1f46df
github username <github email address>
2018-09-19 22:55:46 -0400

    adding WIP2

--- lib/school.rb
+++ lib/school.rb
@@ -12,21 +12,31 @@ end
 def add_student(student_name, grade)
   (roster[grade] ||= []) << student_name
   # p (roster[10] ||= [] << student_name)
+  #Long Way:
+  if roster.has_keys?(grade)
+    roster[grade] << [name]
+  else
+    roster[grade] = [name]
+  end
 
   # binding.pry
 end
 
-def grade
-  if roster[] = [grade]
-  else
-    rooster[grade]  = [student]
-  end 
-end
 
-def sort
-  student_name.each do |a, b|
-    a.sort!
-  end
+def grade
+  roster.select{ |k| k == grade }
+# level_2_employees = ehash["employee"].select {|k| k["level"] == "2"}
+  binding.pry
+  # if roster[] = [grade]
+  # else
+  #   rooster[grade]  = [student]
+  # end
+# end
+#
+# def sort
+#   student_name.each do |a, b|
+#     a.sort!
+#   end
 end
 
 end
